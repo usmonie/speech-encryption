64a550abd64b0700cf4645bb37298e69fa7bcdd6
Usman Akhmedov <akhme.v@gmail.com>
2023-06-17 17:06:21 +0300

    Changed result params

--- src/models.rs
+++ src/models.rs
@@ -9,22 +9,17 @@ pub mod requests {
 
 pub mod result {
     use e521_curve::e521::Point;
-    use num_bigint_dig::BigInt;
 
     pub struct GenerateKeyPairResult {
-        pub x: BigInt,
-        pub y: BigInt,
-
-        pub private_key: Vec<u8>
+        pub public_key: Point,
+        pub secret_key: Vec<u8>,
     }
 
     impl GenerateKeyPairResult {
-        pub fn from(public_point: Point, private_key: Vec<u8>) -> Self {
+        pub fn from(public_key: Point, secret_key: Vec<u8>) -> Self {
             Self {
-                x: public_point.x,
-                y: public_point.y,
-
-                private_key,
+                public_key,
+                secret_key,
             }
         }
     }
